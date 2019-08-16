98c79499338114f524a674591ca75bcf7567c843
Matteo Bartalini <MatteoBartalini1@gmail.com>
2019-08-15 21:38:49 -0400

    added dialog, improved readability, updated readme

--- FindNReplaceNFiles.py
+++ FindNReplaceNFiles.py
@@ -5,76 +5,79 @@ import re
 pluginName = "FindNReplaceNFiles"
 debugOn = True
 
+def pprint(message, addNewLine=False):
+    if debugOn:
+        print("{}: {}".format(pluginName, message))
+        if addNewLine:
+            print()
 
 class ExampleCommand(sublime_plugin.EventListener):
+    # Plugin print for more readability
+
+
+
     def on_pre_close(self, view):
-        if view.name() == "Find Results":
-            # first lets save any files that are chilling
-            # Write out every buffer (active window) with
-            # changes and a file name.
-            window = sublime.active_window()
-            for view in window.views():
-                if view.is_dirty() and view.file_name():
-                    view.run_command('save')
+        if view.name() != "Find Results":
+            pprint("Nothing to do here")
+            return 
 
-            # actual logic
-            contents = view.substr(sublime.Region(0, view.size()))
-            lines = contents.split("\n")
-            currentFileName = ""
-            currentFile = None
-            currentFileLines = []
-            for line in lines:
-                regex4file = "^([\/].*)+:"
-                regex4line = "[\s]*([\d]+)(?:[\:][\s]+|[\s]{2,})(.+|)(?:\n|)"
-                result4line = re.search(regex4line, line)
-                result4file = re.search(regex4file, line)
-                if result4file is not None:  # is a file
-                    groups = result4file.groups()
-                    if debugOn:
-                        print ("FILE:" + line)
-                        print (groups)
-                        print ()
-                    if currentFile is not None:
-                        currentFile.close()
-                        with open(currentFileName, "w") as file:
-                            file.writelines(currentFileLines)
-                    currentFileName = groups[0]
-                    currentFile = open(currentFileName, "r")
-                    currentFileLines = currentFile.readlines()
-                elif result4line is not None:  # is a line
-                    groups = result4line.groups()
-                    if debugOn:
-                        print ("LINE:" + line)
-                        print (groups)
-                        print ()
-                    if currentFile is not None:
+        # When the name is 'Find Results'
+        should_make_changes = sublime.ok_cancel_dialog(
+            "{}: Would you like to inflict any changes on your local copy?".format(pluginName),
+            "Yes")
+        if not should_make_changes:
+            pprint("User chose to not save changes")
+            return 
+            
+        # save any open tabs, so changes don't get mix-matched
+        window = sublime.active_window()
+        for view in window.views():
+            if view.is_dirty() and view.file_name():
+                view.run_command('save')
+
+        # main logic
+        contents = view.substr(sublime.Region(0, view.size()))
+        results_lines = contents.split("\n")
+        current_filename = ""
+        current_file = None
+        current_file_lines = []
+        for line in results_lines:
+            regex4file = "^([\/].*)+:"
+            regex4line = "[\s]*([\d]+)(?:[\:][\s]+|[\s]{2,})(.+|)(?:\n|)"
+            result4line = re.search(regex4line, line)
+            result4file = re.search(regex4file, line)
+            if result4file is not None:  # is a file
+                pprint("FILE:{}".format(line))
+                if current_file is not None:
+                    current_file.close()
+                    with open(current_filename, "w") as file:
+                        file.writelines(current_file_lines)
+                current_filename = result4file.groups()[0]  # set the new filename
+                current_file = open(current_filename, "r")
+                current_file_lines = current_file.readlines()
+            elif result4line is not None:  # is a line
+                groups = result4line.groups()
+                line_num = int(groups[0]) - 1
+                pprint("LINE: {}".format(line))
+                pprint(groups, addNewLine=True)
+                if current_file is not None:  # will always be true 
+                    pprint(line_num)
+                    pprint(current_file_lines)
+                    if line_num < len(current_file_lines):
                         regex4spaces = "^([ \t]*).*"
-                        # grab the line
-                        lineNum = int(groups[0]) - 1
-                        if debugOn:
-                            print(lineNum)
-                            print(currentFileLines)
-                        # grab the whitespace from the line
                         result4spaces = re.search(
-                            regex4spaces, currentFileLines[lineNum])
+                        regex4spaces, current_file_lines[line_num])
                         whitespace = result4spaces.groups()[0]
-                        if lineNum < len(currentFileLines):
-                            currentFileLines[lineNum] = \
-                                whitespace + groups[1] + "\n"
-                        elif lineNum == len(currentFileLines):
-                            currentFileLines.append(
-                                whitespace + groups[1] + "\n")
-                        else:
-                            print(pluginName + ": Error saving line, it's \
-                                beyond the current file length")
-                            print("\tFile: " + currentFileName +
-                                  " on line " + str(lineNum + 1))
-                            print("\tTried to save '" + groups[1] + "'")
-            if currentFile is not None:  # one last save
-                currentFile.close()
-                with open(currentFileName, "w") as file:
-                    file.writelines(currentFileLines)
+                        current_file_lines[line_num] = \
+                            whitespace + groups[1] + "\n"
+                    elif line_num == len(current_file_lines):
+                        current_file_lines.append(groups[1] + "\n")
+                    else:
+                        pprint("Error saving line, it's beyond the current file length")
+                        pprint("File: {} on line {}".format(current_filename, line_num + 1))
+                        pprint("Tried to save '{}'".format(groups[1]))
 
-        else:  # when the buffer that is closing is not a Find Results File
-            if debugOn:
-                print(pluginName + ": Nothing to do here")
+        if current_file is not None:  # one last save
+            current_file.close()
+            with open(current_filename, "w") as file:
+                file.writelines(current_file_lines)
--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 # SublimeFindNReplaceNFiles
-The faster (better) way to use Sublime to Find &amp; Replace in multiple files
+The faster (better) way to use Sublime to Find &amp; Replace in multiple files.
 
-A small Sublime 3 plugin that allows users to quickly Find & Replace right from the `Find in Files` 
-tool. As Uncle Ben once said, 'with great power comes great responsibilty.'
+A lightweight Sublime 3 plugin that allows users to quickly Find & Replace right from the `Find in Files` 
+tool. As Uncle Ben once said, 'with great power comes great responsibility.'
 
-# Use
+## Use
 
-# Note
+To use open a `Find In Files` window (Command + Shift + f on Mac or `Find` on the top bar -> `Find in Files...`). Once the window is open with all of the find results, simply start editing right in the window. Once you try to close the `Find Results` a pop up will come up asking if you would like to save changes. Tapping on yes will write the changes to the file, otherwise tapping on no will ignore any changes. 
 
-# Installation
+Please also read the Notes section below for corner use cases. 
 
-# Why
-I was once asked to make one small change to a large number of files. The task was essentially to do a quick find and replace 
-that spanned about 70 files. Using the Find & Replace in All Files worked okay, but it was more effort than it 
-should have been. It opens up all the relevent files and makes you check each one and close each one individually. Does this 
-work well with 3 files? Yes. Does this work well with 20 file? Ehh. Does it work well with 70 files? Nope. I also had to
-manually revert back a few of the files. Hopefully this plugin is the solution.
+## ~~_Limitations_~~ Notes
+Once the `Find In Files` window is open, do not go back and change the files directly. If any changes are made to the files after the window is open, any changes that are made from the `Find Results` window will *not* be made. 
+
+Files are *automatically* saved after making changes from the `Find Results` window. Any files that are currently opened are also saved.
+
+When making changes to lines that are already tabbed, the plugin will always keep the spacing from the original line, and simply replace just the text. In other words, you can not change preceding spacing from `Find Results` window. 
+
+On a similar note, if writing on a new line (no spacing or characters). Then the line will default to have no preceding spaces. This is especially true if writing on the last line.
+
+## Installation
+To manually add this plugin to Sublime, download the [file](FindNReplaceNFiles.py). Open Sublime -> go to `Preferences` (on Mac go to `Sublime Text` first) -> `Browse Packages` -> Open the folder `User` -> drag the python file `FindNReplaceNFiles.py` (into the folder) -> Enjoy!
+
+By manually adding the file, any changes made to the file will affect the plugin. Feel free to edit the project, and submit a pull request! 
+
+## Why
+I was once asked to make one small change to a large number of files. The task was essentially to do a quick find and replace that spanned about 70 files. Using the Find & Replace in All Files worked okay, but it was more effort than it should have been. It opens up all the relevent files and makes you check each one and close each one individually. Does this work well with 3 files? Yes. Does this work well with 20 files? Ehh. Does it work well with 70 files? Nope. Hopefully this plugin is the solution.
